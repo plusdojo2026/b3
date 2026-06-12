6170f42dd4b7680f89bd87c2f26c65136154526d
plusdojo2026-shimizu <shimizu-hinata-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 16:05:28 +0900

    会員登録・ログイン処理実装
    今後もうちょい修正します

--- source/src/main/java/servlet/LogoutServlet.java
+++ source/src/main/java/servlet/LogoutServlet.java
@@ -25,10 +25,11 @@ public class LogoutServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+    		throws ServletException, IOException {
+
+    	request.getRequestDispatcher("/WEB-INF/jsp/login.jsp").forward(request, response);
+    }
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
