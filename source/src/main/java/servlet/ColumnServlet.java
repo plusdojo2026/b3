f811c6f282ef3eebc9d4cfb7638ddf62844ff46f
plusdojo2026-anzai <anzai-ryoma-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-12 16:06:18 +0900

    コラムjsp,サーブレット一旦完成　DAO途中

--- source/src/main/java/servlet/ColumnServlet.java
+++ source/src/main/java/servlet/ColumnServlet.java
@@ -1,11 +1,16 @@
 package servlet;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import dao.ColumnDao;
+import dto.Column;
 
 /**
  * Servlet implementation class ColumnServlet
@@ -27,9 +32,25 @@ public class ColumnServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
+		// 言語設定
+		String lang = request.getParameter("lang");
+		if (lang == null) {
+			lang = "ja";
+		}
+		request.setAttribute("lang", lang);
+		
+		// DB取得
+		ColumnDao dao = new ColumnDao();
+		List<Column> list = dao.findAll();
+		
+		// JSPに渡す
+		request.setAttribute("list", list);
+		
+		// JSPへフォワード
+		request.getRequestDispatcher("/webapp/WEB-INF/jsp/column.jsp").forward(request, response);
 	}
-
+	
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
