f99b82a9e02e92a140f9e4f688731ae6f7a4c385
plusdojo2026-shimizu <shimizu-hinata-plusdojo2026@seplus2016.onmicrosoft.com>
2026-06-22 10:41:44 +0900

    アラート機能

--- source/src/main/webapp/js/home.js
+++ source/src/main/webapp/js/home.js
@@ -1,9 +1,12 @@
 'use strict';
 
+/* ------------------------- */
+/* --------金額入力欄-------- */
+/* ------------------------- */
+
 const amountInput = document.querySelector('.amount-input');
 
 let isComposing = false;
-
 
 // 全角数字 → 半角数字
 function toHalfWidthNumber(value) {
@@ -14,6 +17,10 @@ function toHalfWidthNumber(value) {
 
 // 金額表示用に整形
 function formatAmount() {
+	if (amountInput == null) {
+		return;
+	}
+
 	let value = amountInput.value;
 
 	// 全角数字を半角数字に変換
@@ -60,7 +67,7 @@ if (amountInput != null) {
 		formatAmount();
 	});
 
-	if (amountInput.form) {
+	if (amountInput.form != null) {
 		amountInput.form.addEventListener('submit', function() {
 			let value = amountInput.value;
 
@@ -72,34 +79,50 @@ if (amountInput != null) {
 	}
 }
 
+/* ----------------------- */
+/* --------財布開閉-------- */
+/* ----------------------- */
 
-// 財布メニュー開閉
 const eyeBtn = document.getElementById("eyeBtn");
 const moneyListView = document.getElementById("wallet-money-list");
 const homeWalletAmount = document.getElementById("home-wallet-amount");
-const orignalWalletAmount = homeWalletAmount.textContent;
-homeWalletAmount.textContent = "~~~~~~"
 
-if (eyeBtn) {
+if (eyeBtn != null && moneyListView != null && homeWalletAmount != null) {
+	const originalWalletAmount = homeWalletAmount.textContent;
+
+	// 初期状態は金額を隠す
+	homeWalletAmount.textContent = "~~~~~~";
+
 	eyeBtn.addEventListener("click", function() {
 		moneyListView.classList.toggle("open");
 
 		if (moneyListView.classList.contains("open")) {
 			// open が付いた時
 			eyeBtn.textContent = "閉";
-			homeWalletAmount.textContent = orignalWalletAmount;
+			homeWalletAmount.textContent = originalWalletAmount;
 		} else {
 			// open が外れた時
 			eyeBtn.textContent = "目";
-			homeWalletAmount.textContent = "~~~~~~"
+			homeWalletAmount.textContent = "~~~~~~";
 		}
 	});
 }
-/*  予算アラート機能  */
 
-let alertHomeAmount = Number(homeWalletAmount.textContent.replace(/[^0-9]/g, ''));
+/* ------------------------- */
+/* --------予算アラート-------- */
+/* ------------------------- */
 
-if()
+const alertHomeAmount = document.getElementById("total-amount");
+const alertHomeCount = document.getElementById("coin-amount");
+const message1 = document.getElementById("massage1");
 
+if (alertHomeAmount != null && alertHomeCount != null && message1 != null) {
+	const homeCashAmount = Number(alertHomeAmount.textContent.replace(/[^0-9]/g, ''));
+	const homeCoinCount = Number(alertHomeCount.textContent.replace(/[^0-9]/g, ''));
 
-
+	if (homeCashAmount < 10000) {
+		message1.textContent = "予算が少なくなってきたでござる";
+	} else if(homeCoinCount > 20) {
+		message1.textContent = "小銭が多くなってきたでござる";
+	}
+}
